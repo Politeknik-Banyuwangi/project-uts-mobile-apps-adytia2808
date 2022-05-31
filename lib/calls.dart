a4e79e10d86c71cfaadffb5a8117b5658c725f59
FlutLab.io <>
2022-05-31 04:33:23 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -18,14 +18,14 @@ class Calls extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-
-            // Masukkan Coding disini jangan rubah lainnya
-            //
-            //
-            //
-            //
-            //
-            //
+            callCard("Raiden Shogun", Icons.call_missed, Colors.red, "July 18, 14:02", NetworkImage("https://berita.yodu.id/wp-content/uploads/2022/01/Header.webp")),
+            callCard("Gojo Satoru", Icons.call_made, Colors.green, "July 02, 18:33", NetworkImage("https://www.greenscene.co.id/wp-content/uploads/2021/09/Jujutsu-Kaisen-4.jpg")),
+            callCard("Kamisato Ayato", Icons.call_received, Colors.green, "Mei 31, 07:15", NetworkImage("https://gamerwk.sgp1.cdn.digitaloceanspaces.com/2022/03/Ayato-Best-Build.jpg")),
+            callCard("Kirigaya Kazuto", Icons.call_made, Colors.green, "August 12, 18:33", NetworkImage("https://animeanalyst.files.wordpress.com/2016/07/kirito.png")),
+            callCard("Gojo Satoru", Icons.call_missed, Colors.red, "Feb 19, 10:33", NetworkImage("https://www.greenscene.co.id/wp-content/uploads/2021/09/Jujutsu-Kaisen-4.jpg")),
+            callCard("Gojo Satoru", Icons.call_received, Colors.green, "Feb 02, 12:33", NetworkImage("https://www.greenscene.co.id/wp-content/uploads/2021/09/Jujutsu-Kaisen-4.jpg")),
+            callCard("Kamisato Ayato", Icons.call_made, Colors.green, "Jan 28, 22:15", NetworkImage("https://gamerwk.sgp1.cdn.digitaloceanspaces.com/2022/03/Ayato-Best-Build.jpg")),
+            callCard("Kamisato Ayaka", Icons.call_missed, Colors.red, "July 18, 14:02", NetworkImage("https://foto.kontan.co.id/eQpBGeMMmMkhHSwU5oO1RAn-Gjw=/smart/2021/07/15/2100661850p.jpg")),
           ],
         ),
       ),
@@ -53,6 +53,41 @@ class Calls extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget callCard(String name, IconData iconData, Color iconColor, String Time, NetworkImage networkImage) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 0.9),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: networkImage,
+          radius: 30,
+        ),
+        title: Text(
+          name,
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
+        subtitle: Row(
+          children: [
+            Icon(
+              iconData,
+              color: iconColor,
+              size: 20,
+            ),
+            SizedBox(width: 6),
+            Text(
+              Time,
+              style: TextStyle(fontSize: 13),
+            ),
+          ],
+        ),
+        trailing: Icon(
+          Icons.call,
+          size: 28,
+          color: Colors.teal,
+        ),
       ),
     );
   }
