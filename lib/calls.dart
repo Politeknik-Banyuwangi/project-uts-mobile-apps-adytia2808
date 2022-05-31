0f9e88ee99dcbac20a3e2a8a42235b6555098f9c
FlutLab.io <>
2022-05-31 05:25:38 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -21,7 +21,7 @@ class Calls extends StatelessWidget {
             callPhone("Ayang", Icons.call_received, Colors.green, "Mei 20, 12:08", NetworkImage("https://i.ibb.co/BwqbJxg/ayang.jpg")),
             callPhone("Sokles Ayam", Icons.call_received, Colors.green, "Mei 20, 20:45", NetworkImage("https://i.ibb.co/QfpJXCq/pp2.jpg")),
             callPhone("Wahyu", Icons.call_received, Colors.green, "Mei 25, 06:30", NetworkImage("https://i.ibb.co/MSqdrmV/pp.jpg")),
-            callPhone("Ali", Icons.call_made, Colors.green, "Mei 25, 08:25", NetworkImage("https://cdn.idntimes.com/content-images/post/20180920/4faca301c0d25bbfa08bb1256a1c71e2.jpg")),
+            callPhone("Ali", Icons.call_made, Colors.green, "Mei 25, 08:25", NetworkImage("https://i.ibb.co/MSqdrmV/pp.jpg")),
             callPhone("Ayu Ginanjar", Icons.call_missed, Colors.red, "Mei 25, 12:03", NetworkImage("https://i.ibb.co/MSqdrmV/pp.jpg")),
             callPhone("Siti Stikus", Icons.call_received, Colors.green, "Mei 25, 13:55", NetworkImage("https://i.ibb.co/MSqdrmV/pp.jpg")),
             callPhone("Putri Rahayu", Icons.call_missed, Colors.red, "Mei 25, 21:15", NetworkImage("https://i.ibb.co/MSqdrmV/pp.jpg")),
